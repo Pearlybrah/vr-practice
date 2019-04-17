3663ac10643427fadfd5d98e12d6860fbdb37a97
Zachary Ferreira <Pearlybrah@github.com>
2019-04-16 18:45:31 -0700

    fully functional

--- office/src/App.js
+++ office/src/App.js
@@ -1,37 +1,71 @@
 import React from "react";
-import { asset, Pano, Text, View, AmbientLight, Sound } from "react-vr";
+import {
+  asset,
+  Pano,
+  Text,
+  View,
+  AmbientLight,
+  Sound,
+  VrButton
+} from "react-vr";
 import Penholder from "./Penholder";
+
 export default class App extends React.Component {
+  state = {
+    background: "fancy.webp",
+    sound: "officerain.wav",
+    visible: true
+  };
+
+  renderItems = () => {
+    if (this.state.visible) {
+      return <Penholder />;
+    }
+  };
+
+  changeScene = () => {
+    this.setState({
+      background: "outside.jpeg",
+      sound: "birds.wav",
+      visible: false
+    });
+  };
   render() {
     return (
       <View>
         <AmbientLight intensity={2.5} />
-        <Pano source={asset("fancy.webp")} />
+        <Pano source={asset(this.state.background)} />
         <Sound
           loop={true}
           volume={0.7}
           autoplay={true}
           source={{
-            wav: asset("officerain.wav")
+            wav: asset(this.state.sound)
           }}
         />
-        <Text
-          style={{
-            color: "#000",
-            fontSize: 0.8,
-            fontWeight: "400",
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: "center",
-            textAlignVertical: "center",
-            transform: [{ translate: [0, 2, -6] }]
-          }}
-        >
-          Take a look around...
-        </Text>
-        <Penholder />
+        <VrButton onClick={this.changeScene}>
+          <Text
+            style={{
+              backgroundColor: "transparent",
+              position: "absolute",
+              color: "#000",
+              fontSize: 0.8,
+              fontWeight: "400",
+              layoutOrigin: [0.5, 0.5],
+              paddingLeft: 0.2,
+              paddingRight: 0.2,
+              textAlign: "center",
+              textAlignVertical: "center",
+              transform: [{ translate: [0, 2, -6] }]
+            }}
+          >
+            Click here to go outside...
+          </Text>
+        </VrButton>
+        {this.renderItems}
       </View>
     );
   }
 }
+
+//ipconfig getifaddr (INSERT IP ADDRESS FOR HOST ADDRESS)
